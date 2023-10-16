3891cdc5391d92e803da86fa0358e97fca765318
Rishi2k3 <118542258+Rishi2k3@users.noreply.github.com>
2023-10-17 00:46:24 +0530

    unsorted linked list 0(n2)

--- oops/linked_list_lec_44/unsorted_linklist.cpp
+++ oops/linked_list_lec_44/unsorted_linklist.cpp
@@ -1,36 +1,37 @@
 // Remove Duplicates From an Unsorted Linked List
 
-#include <iostream>
-
-Node *removeDuplicates(Node *head)
-{
-   if(head==NULL)
-   {
-       return NULL;
-   }
-
-   Node * curr= head;
-   while(curr!=NULL)
-   {
-       Node* temp= curr->next;
-       Node* prev=curr;
-
-       while(temp!=NULL)
-       {
-           if(curr->data==temp->data)
-           {
-               Node* nodetodelete= temp;
-               prev->next= temp->next;
-               temp=temp->next;
-               delete nodetodelete;
-           }
-           else
-           {
-               prev=temp;
-               temp=temp->next;
-           }
-       }
-       curr=curr->next;
-   }
-   return head;
+Node *removeDuplicates(Node *head){
+ 
+    if(head==NULL) {
+        return head;
+    }
+ 
+    Node* curr = head;
+ 
+    while(curr != NULL) {
+ 
+        Node* temp = curr;
+ 
+        while(temp->next!= NULL) {
+            
+            if(curr -> data == temp -> next -> data) {
+ 
+                Node* next_next = temp -> next -> next;
+                Node* nodeToDelete = temp -> next;
+                delete(nodeToDelete);
+                temp -> next = next_next;
+ 
+            }
+            else{
+                temp = temp -> next;
+            }            
+ 
+        }
+ 
+        curr = curr -> next;
+ 
+    }
+ 
+    return head;
+    
 }
